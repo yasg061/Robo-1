9fe669fddd7dc9ccc544e5008f224ad5f404bf40
Jonathan Jacob Mejía Flores <Jonthan099902@gmail.com>
2020-02-04 18:04:00 -0600

    code update

--- CodR/CodR.ino
+++ CodR/CodR.ino
@@ -2,59 +2,92 @@ int dirm = 2;
 int pulm = 3;
 int dirmd = 5;
 int pulmd = 6;
+int pushButton = 8;
+const int ledPin = 13;
+volatile int value;
+char c;
+
 
 void setup() {
   pinMode(pulm, OUTPUT);
   pinMode(dirm, OUTPUT);
   pinMode(pulmd, OUTPUT);
   pinMode(dirmd, OUTPUT);
+  pinMode(pushButton, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  //--------------ARRIBA-------------------
+  
+  value = digitalRead(pushButton);
+
+  if(value==HIGH){
+    Serial.begin(9600);
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+
+    if(Serial.available()){
+    c = Serial.read();
+    
+    //-------------UP PEDAL---------------
+    //-- UP PEDAL ACTIVATED
+    if(c == 'A'){
+    
+      digitalWrite(dirm, HIGH);
+      delayMicroseconds(10);
+       
+      for(int j=0; j<35; j++){
+      digitalWrite(pulm, HIGH);
+      delayMicroseconds(6500);
+      digitalWrite(pulm, LOW);
+        }
+      } 
+    
+    //-- UP PEDAL DISABLED
+      else if( c== 'B' ){
+
+      digitalWrite(dirm, LOW);
+      delayMicroseconds(10);
+      
+      for(int i=0; i<35; i++){
+        digitalWrite(pulm, HIGH);
+        delayMicroseconds(6500);
+        digitalWrite(pulm, LOW);
+       }
+      }
+
+    //------------DOWN PEDAL----------------
+    //-- DOWN PEDAL DISABLED
+      else if(c=='C'){
  
-  digitalWrite(dirm, LOW);
-  delayMicroseconds(10);
-
-  for(int j=0; j<200; j++){
-    digitalWrite(pulm, HIGH);
-    delayMicroseconds(1500);
-    digitalWrite(pulm, LOW);
-  }
-  
-  delay(1000);
-  
-  digitalWrite(dirm, HIGH);
-  delayMicroseconds(10);
-
-  for(int i=0; i<200; i++){
-    digitalWrite(pulm, HIGH);
-    delayMicroseconds(1500);
-    digitalWrite(pulm, LOW);
-  }
-  delay(1000);
- 
-  //---------------ABAJO-------------------
-
-  digitalWrite(dirmd, LOW);
-  delayMicroseconds(10);
-
-  for(int j=0; j<50; j++){
-    digitalWrite(pulmd, HIGH);
-    delayMicroseconds(1500);
-    digitalWrite(pulmd, LOW);
-  }
-  
-  delay(1000);
-  
-  digitalWrite(dirmd, HIGH);
-  delayMicroseconds(10);
-
-  for(int i=0; i<200; i++){
-    digitalWrite(pulmd, HIGH);
-    delayMicroseconds(1500);
-    digitalWrite(pulmd, LOW);
-  }
-  delay(1000);
- 
+      digitalWrite(dirmd, LOW);
+      delayMicroseconds(10);
+    
+      for(int j=0; j<35; j++){
+        digitalWrite(pulmd, HIGH);
+        delayMicroseconds(6500);
+        digitalWrite(pulmd, LOW);
+      }
+    }
+    
+    //-- DOWN PEDAL ACTIVATED
+    else if(c=='D'){
+      
+        digitalWrite(dirmd, HIGH);
+        delayMicroseconds(10);
+      
+        for(int i=0; i<35; i++){
+          digitalWrite(pulmd, HIGH);
+          delayMicroseconds(6500);
+          digitalWrite(pulmd, LOW);
+        }
+     }
+   } 
+    }
+    else{
+      digitalWrite(ledPin, LOW);
+      Serial.end();
+      
+    }
 }
