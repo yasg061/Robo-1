d59a6ce9f6e4a6a2e0e73a722095280375eb30a0
Jonathan Flores <jonathan099902@gmail.com>
2020-03-06 14:52:52 -0600

    update

--- pedalControl/pedalControl.ino
+++ pedalControl/pedalControl.ino
@@ -2,10 +2,6 @@ const int PIN_DIR = 5;
 const int PIN_PUL = 6;
 const int PIN_DIR2 = 9;
 const int PIN_PUL2 = 10;
-const int PIN_RELAY = 2;
-const int PIN_RELAY2 = 3;
-const int PIN_5V = A0;
-
 char value;
 
 void setup() {
@@ -13,21 +9,15 @@ void setup() {
   pinMode(PIN_PUL, OUTPUT);
   pinMode(PIN_DIR2, OUTPUT);
   pinMode(PIN_PUL2, OUTPUT);
-  pinMode(PIN_RELAY, OUTPUT);
-  pinMode(PIN_RELAY2, OUTPUT);
-  pinMode(PIN_5V, OUTPUT);
   Serial.begin(9600);
+  
 }
 
 void loop() {
-  digitalWrite(PIN_5V, HIGH);
-
+  
   if(Serial.available()){
-    value = Serial.read();
-    
-    digitalWrite(PIN_RELAY, HIGH);
-    digitalWrite(PIN_RELAY2, HIGH);
-       
+    value = Serial.read(); 
+
     if(value == 'A'){
       
       digitalWrite(PIN_DIR, HIGH);
@@ -56,20 +46,8 @@ void loop() {
       delayMicroseconds(6500);
       digitalWrite(PIN_PUL2, LOW);
       
-    } else if(value == 'E'){
-      
-      digitalWrite(PIN_RELAY, HIGH);
-      digitalWrite(PIN_RELAY2, HIGH);
-      
-    } else if(value == 'F'){
-      digitalWrite(PIN_RELAY, LOW);
-      digitalWrite(PIN_RELAY2, LOW);
-    } else {
+    }else {
       
     }
-  } else{
-    digitalWrite(PIN_RELAY, LOW);
-    digitalWrite(PIN_RELAY2, LOW);
   }
-
 }
